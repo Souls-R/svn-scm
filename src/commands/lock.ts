4d9eeb91070e840e40df6a1e7e1bd06fbc5f81a3
Chris Bachhuber <chris.bachhuber@eu.socionext.com>
2026-02-16 10:26:55 +0100

    feat: support binary files also for getting a file URL

--- src/commands/lock.ts
+++ src/commands/lock.ts
@@ -1,11 +1,4 @@
-import {
-  TabInputCustom,
-  TabInputNotebook,
-  TabInputText,
-  TabInputTextDiff,
-  Uri,
-  window
-} from "vscode";
+import { Uri, window } from "vscode";
 import { Command } from "./command";
 
 export class Lock extends Command {
@@ -49,41 +42,5 @@ export class Lock extends Command {
         window.showErrorMessage(`Unable to lock file: ${error}`);
       }
     });
-  }
-
-  private getUriFromActiveTab(): Uri | undefined {
-    const activeTab = window.tabGroups.activeTabGroup?.activeTab;
-    const input = activeTab?.input;
-
-    if (!input) {
-      return;
-    }
-
-    if (input instanceof TabInputText) {
-      return input.uri;
-    }
-
-    if (input instanceof TabInputCustom) {
-      return input.uri;
-    }
-
-    if (input instanceof TabInputNotebook) {
-      return input.uri;
-    }
-
-    if (input instanceof TabInputTextDiff) {
-      if (input.modified.scheme === "file") {
-        return input.modified;
-      }
-
-      if (input.original.scheme === "file") {
-        return input.original;
-      }
-
-      return;
-    }
-
-    const inputWithUri = input as { uri?: Uri };
-    return inputWithUri.uri;
   }
 }
